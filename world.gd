f450745e3cfbdcec3d50f1ead0b1a9aac4e0b449
RunningGriffin <runninggriffin@gmail.com>
2023-10-21 14:02:34 -0600

    stable fr fr

--- world.gd
+++ world.gd
@@ -21,5 +21,6 @@ func _on_enemy_2_hit():
 	$GameOverScreen.show()
 
 func _on_player_win():
-	get_tree().change_scene_to_file("res://victory.tscn")
+	$victoryScreen.setValues()
+	$victoryScreen.show()
 
