183ecb66a06d3b7908dc9079e9aee2d3660174fd
Jacob Holst <hol19002@byui.edu>
2023-10-21 08:56:57 -0600

    Coin detection detected by our diligent detective

--- coin.gd
+++ coin.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
+class_name coin
+
+@onready var player = $"../Player"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("animated")
 
+func _process(delta):
+	if player.position == position:
+		player.getCoin()
+		queue_free()
