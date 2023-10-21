645d471468f445d0fc3751c73bc015338daa49cc
Colby Hale <28745762+colh99@users.noreply.github.com>
2023-10-20 23:39:05 -0600

    added actionsInterface to world scene

--- possibleActionsItemList.gd
+++ possibleActionsItemList.gd
@@ -1,9 +1,22 @@
 extends ItemList
 
 
+var action_move_toggle = false
+var action_turn_left_toggle = false
+var action_turn_right_toggle = false
+
+
 func _on_item_activated(index):
 	match index:
 		0: # Move
 			print("Move")
-		1: # Turn
-			print("Turn")
+			
+			
+		1: # Turn Left
+			print("Turn Left")
+			
+			
+		2: # Turn Right
+			print("Turn Right")
+			
+
